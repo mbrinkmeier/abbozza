40dd531f1d8c6a253dd0ff4be447b119f704d679
michael <michael@ridcully>
2017-01-15 15:23:58 +0100

    0.8.1

--- src/de/uos/inf/did/abbozza/AbbozzaServer.java
+++ src/de/uos/inf/did/abbozza/AbbozzaServer.java
@@ -79,8 +79,8 @@ public abstract class AbbozzaServer implements HttpHandler {
     // Version
     public static final int VER_MAJOR = 0;
     public static final int VER_MINOR = 8;
-    public static final int VER_REV = 0;
-    public static final String VER_REM = "(calliope)";
+    public static final int VER_REV = 1;
+    public static final String VER_REM = "";
     public static final String VERSION = "" + VER_MAJOR + "." + VER_MINOR + "." + VER_REV + " " + VER_REM;
 
     // Instance
